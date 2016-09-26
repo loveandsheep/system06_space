0a607f5823301bf5dbfb05b9c053ea7e37088897
Ovis aries <sheep@tele-scope.jp>
2016-09-26 12:43:31 +0900

    first commit

---
+++ src/roomManager.cpp
@@ -0,0 +1,9 @@
+//
+//  roomManager.cpp
+//  system06_space
+//
+//  Created by Ovis aries on 2016/09/25.
+//
+//
+
+#include "roomManager.h"
---
+++ src/roomManager.h
@@ -0,0 +1,27 @@
+//
+//  roomManager.h
+//  system06_space
+//
+//  Created by Ovis aries on 2016/09/25.
+//
+//
+
+#ifndef roomManager_h
+#define roomManager_h
+
+class rmUnit{
+public:
+	
+	void pop();
+	
+	bool ballStat;
+};
+
+class roomManager{
+public:
+	
+	void setup();
+	
+};
+
+#endif /* roomManager_h */
