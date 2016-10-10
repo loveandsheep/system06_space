c39c9de579fd30608a7a566032d7f91c5292a005
Ovis aries <culminate0523@gmail.com>
2016-10-10 16:58:59 +0900

    all channel

--- src/roomManager.cpp
+++ src/roomManager.cpp
@@ -92,12 +92,9 @@ void roomManager::update()
 	int r = cnt % getNumRow();
 	int c = cnt / getNumRow() % getNumColumn();
 	
-	if (units[r][c].curAnalog > analog_thr)
-	{
-		cout << "bang " << r << "," << c << endl;
-		cout << units[r][c].curAnalog << endl;
-		bang(r, c);
-	}
+	cout << "bang " << r << "," << c << endl;
+	cout << units[r][c].curAnalog << endl;
+	bang(r, c);
 }
 
 void roomManager::draw()
